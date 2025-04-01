968aa77d858f432342b02145b5775e575925455a
Vidyesh Churi <vidyesh95@gmail.com>
2025-04-01 21:50:02 +0530

    feat: add dashboard pages and enhance UI components
    - Added new dashboard pages for customers, invoices, and main dashboard
    - Enhanced layout and navigation components with dynamic styling
    - Integrated fonts and updated global styles for better readability
    - Added hero images and improved the homepage layout

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Lusitana, Roboto, Roboto_Serif } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] });
+export const roboto = Roboto({ weight: ['400', '700'], subsets: ['latin'] });
+export const robotoSerif = Roboto_Serif({ weight: ['400', '700'], subsets: ['latin'] });
